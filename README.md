d46a02a76c79343953f08bdeeb0c54238736415c
chilot <https://github.com/Raigyo>
2021-02-19 10:51:04 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Socket I/O
+
+*February 2021*
+
+> 🔨 From udemy '[Socket.IO : Créer une discussion instantanée de A à Z](https://www.udemy.com/course/socketio/)'.
+
+![Node / Socket IO Logo](_readme-img/node-socket.png)
+
+## Dependancies
+
+### Dev
+
+[nodemon](https://www.npmjs.com/package/nodemon): nodemon is a tool that helps develop node.js based applications by automatically restarting the node application when file changes in the directory are detected.
+
+`npm i --save-dev nodemon`
+
+[morgan](https://www.npmjs.com/package/morgan): HTTP request logger middleware for node.js
+
+`npm i --save-dev morgan`
---
+++ app.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+const morgan = require("morgan");
+// Default path for sendFile
+let options = {
+  root: __dirname + "/views", // __dirname: current directory
+};
+
+// app.use([path,] callback [, callback...])
+// Mounts the specified middleware function or functions at the specified path:
+// the middleware function is executed when the base of the requested path matches path.
+app.use((req, res, next) => {
+  console.log(`URL: ${req.path}`);
+  next();
+});
+
+app.use(morgan("dev"));
+
+// middleware used to define static files directory
+app.use(express.static(options.root));
+
+// app.get(path, callback): get route
+app.get("/", (req, res) => {
+  res.redirect("home");
+});
+app.get("/home", (req, res) => {
+  // console.log(req.hostname, req.path);
+  // console.log(req.query); // /?page=test => { page: 'test' }
+  // res.send("Hello World!");
+  res.sendFile("index.html", options); // Transfers the file at the given path.
+});
+
+app.get("/params/:name", (req, res) => {
+  // res.send(req.params.name); // display path after params/
+});
+
+// app.listen(path, [callback]):
+// Starts a UNIX socket and listens for connections on the given path.
+// This method is identical to Node’s http.Server.listen().
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
